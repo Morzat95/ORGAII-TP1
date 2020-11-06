8276929fc4dd068d2a9e6a830ba19e557e8b4676
Morzat95 <fe_de_qui9@hotmail.com>
2020-11-06 00:12:08 -0300

    Add X2 result

--- resolvente.asm
+++ resolvente.asm
@@ -3,8 +3,7 @@ extern printf
 
 section .data
 
-    ; format db "a: %.2f, b: %.2f, c: %.2f",10,0
-    format db "result X1 = %.4f",10,0
+    format db "result X1 = %.4f, X2 = %.4f",10,0
     a dq 0
     b dq 0
     c dq 0
@@ -12,6 +11,7 @@ section .data
     menosuno dd -1.0
     dos dd 2.0
     x1 dq 0
+    x2 dq 0
 
 section .text
 global CMAIN             ; must be declared for llsinker (1d)
@@ -22,17 +22,7 @@ CMAIN:                       ; tell the linker entry point
     push ebp
     mov ebp, esp
 
-    ; mov eax, [ebp+8] ; argument 1
-    ; add eax, [esp+4] ; argument 2
-    ; ; a
-    ; fld dword[ebp+8]
-    ; fst qword[result]
-
-    ; ; b
-    ; fld dword[ebp+12]
-    ; fst qword[b]
-
-    ; c
+    ; ---- CALCULO X1 ----
     ; -b
     fld dword[ebp+12]
     fld dword[menosuno]
@@ -67,15 +57,54 @@ CMAIN:                       ; tell the linker entry point
     ; [ -b + (b^2-4*a*c)^1/2 ] / 2*a
     fdiv
 
-    ; resultado X1
-    fst qword[x1]
+    ; resultado -> X1 (el stack de la FPU está vacío ahora)
+    fstp qword[x1]
+
+    ; ---- CALCULO X2 ----
+    ; -b
+    fld dword[ebp+12]
+    fld dword[menosuno]
+    fmul
+
+    ; b^2
+    fld dword[ebp+12]
+    fld dword[ebp+12]
+    fmul
+
+    ;4*a*c 
+    fld dword[cuatro]
+    fld dword[ebp+8]
+    fld dword[ebp+16]
+    fmul
+    fmul
+
+    ; b^2-4*a*c
+    fsub
+
+    ; (b^2-4*a*c)^1/2
+    fsqrt
+
+    ; -b - (b^2-4*a*c)^1/2
+    fsub
+
+    ; 2*a
+    fld dword[dos]
+    fld dword[ebp+8]
+    fmul
+
+    ; [ -b + (b^2-4*a*c)^1/2 ] / 2*a
+    fdiv
+
+    ; resultado -> X2 (el stack de la FPU está vacío ahora)
+    fstp qword[x2]
 
     ; print results
+    push dword[x2+4]
+    push dword[x2]
     push dword[x1+4]
     push dword[x1]
     push format
     call printf
-    ; add esp, 28
 
     mov esp, ebp
     pop ebp
